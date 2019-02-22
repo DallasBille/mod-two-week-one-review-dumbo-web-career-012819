209d04317c304b2ac8fab6d8fd4a051a4c5fc381
github username <github email address>
2019-02-22 17:50:31 -0500

    Seven Seas

--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+resources :seas, only: [:index,:show,:edit,:new]
+    #get '/super_secret_route', to: 'seas#super_secret_method'
+    #creates a route(super secret), goes to seascontroller, runs super_secret_method.
 end
